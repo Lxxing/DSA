c183b50854706d574d0978cf668b419c5b25ce09
luxiang <luxiang@tongtech.com>
2020-03-11 11:07:03 +0800

    66.加一

---
+++ LeetCode/L66.cpp
@@ -0,0 +1,90 @@
+/******************************************************************************************
+* Data Structures in C++
+* Auther : luxiang
+* github:https://github.com/Lxxing?tab=repositories
+* 欢迎交流指正
+66.加一
+
+******************************************************************************************/
+
+#include "LeetCode.h"
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+    	for (int i = digits.size() - 1; i >= 0; --i)
+		{
+			digits[i]++;
+			if (digits[i] <= 9 )
+			{
+				return digits;
+			}
+			digits[i] = 0;
+		}
+		if (digits[0] == 0)//题目中的前提判断条件
+		{
+			digits.insert(digits.begin(),1);
+		}
+		return digits;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+	//[8,9,9,9]
+    string line;
+    while (getline(cin, line)) {
+        vector<int> digits = stringToIntegerVector(line);
+        
+        vector<int> ret = Solution().plusOne(digits);
+
+        string out = integerVectorToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
+
+
+
