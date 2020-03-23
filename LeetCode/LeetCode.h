2b102161e222ade3007882a702de48d9922df89d
luxiang <luxiang@tongtech.com>
2020-03-23 08:08:45 +0800

    876. 链表的中间结点

--- LeetCode/LeetCode.h
+++ LeetCode/LeetCode.h
@@ -22,7 +22,18 @@
 using namespace std;
 
 
+struct TreeNode {
+	  int val;
+	  TreeNode *left;
+	  TreeNode *right;
+	  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 
+struct ListNode {
+	  int val;
+	  ListNode *next;
+	  ListNode(int x) : val(x), next(NULL) {}
+};
 
 #endif //__LEET_CODE__H_
 
