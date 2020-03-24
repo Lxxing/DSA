5ce4954a9475a05a3bdedf9e62e1eddd32ec8c70
luxiang <luxiang@tongtech.com>
2020-03-24 17:53:42 +0800

    83. 删除排序链表中的重复元素

---
+++ LeetCode/L83.cpp
@@ -0,0 +1,105 @@
+/******************************************************************************************
+* Data Structures in C++
+* Auther : luxiang
+* github:https://github.com/Lxxing?tab=repositories
+* 欢迎交流指正
+83. 删除排序链表中的重复元素
+******************************************************************************************/
+#include "LeetCode.h"
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+		ListNode *result = head;
+		if (NULL == head)
+		{
+			return result;
+		}
+    	
+        ListNode *pTailer = head->next;
+		while (pTailer)
+		{
+			if (pTailer->val == head->val)
+			{
+				head->next = pTailer->next;//跳过当前点
+			}
+			else
+			{
+				head = head->next;
+			}
+			pTailer = pTailer->next; 
+		}
+		return result;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+string listNodeToString(ListNode* node) {
+    if (node == nullptr) {
+        return "[]";
+    }
+
+    string result;
+    while (node) {
+        result += to_string(node->val) + ", ";
+        node = node->next;
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        ListNode* head = stringToListNode(line);
+        
+        ListNode* ret = Solution().deleteDuplicates(head);
+
+        string out = listNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
+
+
