f235c324983f67eeb1e1861cc72e155f5baa29dc
luxiang <luxiang@tongtech.com>
2020-04-08 11:22:48 +0800

    L844.比较含退格的字符串.cpp

---
+++ LeetCode/L844.比较含退格的字符串.cpp
@@ -0,0 +1,106 @@
+/******************************************************************************************
+* Data Structures in C++
+* Auther : luxiang
+* github:https://github.com/Lxxing?tab=repositories
+* 欢迎交流指正
+******************************************************************************************/
+#include "LeetCode.h"
+
+class Solution {
+public:
+    bool backspaceCompare(string S, string T) {
+		stack<char> stackS;
+		stack<char> stackT;
+		for (int i = 0; i < S.size(); ++i)
+		{
+			if (S[i] == '#')
+			{
+				if(stackS.size()) stackS.pop();
+			}
+			else
+			{
+				stackS.push(S[i]);
+			}
+		}
+		for (int i = 0; i < T.size(); ++i)
+		{
+			if (T[i] == '#')
+			{
+				if(stackT.size()) stackT.pop();
+			}
+			else
+			{
+				stackT.push(T[i]);
+			}
+		}
+
+		if (stackS.size() != stackT.size())
+		{
+			return false;
+		}
+		else
+		{
+			
+			while(stackS.size())
+			{
+				if (stackS.top() == stackT.top())
+				{
+					stackS.pop();
+					stackT.pop();
+				}
+				else
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+    }
+};
+
+string stringToString(string input) {
+    assert(input.length() >= 2);
+    string result;
+    for (int i = 1; i < input.length() -1; i++) {
+        char currentChar = input[i];
+        if (input[i] == '\\') {
+            char nextChar = input[i+1];
+            switch (nextChar) {
+                case '\"': result.push_back('\"'); break;
+                case '/' : result.push_back('/'); break;
+                case '\\': result.push_back('\\'); break;
+                case 'b' : result.push_back('\b'); break;
+                case 'f' : result.push_back('\f'); break;
+                case 'r' : result.push_back('\r'); break;
+                case 'n' : result.push_back('\n'); break;
+                case 't' : result.push_back('\t'); break;
+                default: break;
+            }
+            i++;
+        } else {
+          result.push_back(currentChar);
+        }
+    }
+    return result;
+}
+
+string boolToString(bool input) {
+    return input ? "True" : "False";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        string S = stringToString(line);
+        getline(cin, line);
+        string T = stringToString(line);
+        
+        bool ret = Solution().backspaceCompare(S, T);
+
+        string out = boolToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
+
+
