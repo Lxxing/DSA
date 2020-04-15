ad763d727e1dac883e48141d1a8fe549b519dd00
luxiang <luxiang@tongtech.com>
2020-04-15 18:29:11 +0800

    L257.二叉树的所有路径

---
+++ LeetCode/L257.二叉树的所有路径.cpp
@@ -0,0 +1,111 @@
+/******************************************************************************************
+* Data Structures in C++
+* Auther : luxiang
+* github:https://github.com/Lxxing?tab=repositories
+* 欢迎交流指正
+******************************************************************************************/
+#include "LeetCode.h"
+
+
+class Solution {
+public:
+	void DFS(TreeNode* node,vector<string> & result,string traval)
+	{
+		if (!node)
+		{
+			return;
+		}
+		if (node && !node->left && !node->right)
+		{
+			traval += to_string(node->val);
+			result.push_back(traval);
+			return;
+		}
+
+		traval += to_string(node->val) + "->";
+		DFS(node->left,result,traval);
+		DFS(node->right,result,traval);
+	}
+    vector<string> binaryTreePaths(TreeNode* root) {
+    	vector<string> result;
+		if (!root)
+		{
+			return result;
+		}
+		string traval;
+		DFS(root,result,traval);
+		return result;
+    }
+};
+
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+        
+        Solution().binaryTreePaths(root);
+
+    }
+    return 0;
+}
+
+
