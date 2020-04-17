06637b89dcba7ed1131914c533e05509fc358e96
luxiang <luxiang@tongtech.com>
2020-04-17 11:35:02 +0800

    L1022.从根到叶的二进制数之和

---
+++ LeetCode/L1022.从根到叶的二进制数之和.cpp
@@ -0,0 +1,111 @@
+/******************************************************************************************
+* Data Structures in C++
+* Auther : luxiang
+* github:https://github.com/Lxxing?tab=repositories
+* 欢迎交流指正
+******************************************************************************************/
+#include "LeetCode.h"
+
+class Solution {
+public:
+    int sumRootToLeaf(TreeNode* root) {
+		int result = 0;
+		
+		stack<pair<TreeNode*,int> > travalQ;
+		int num = 0;
+		while (travalQ.size() || root)
+		{
+			while (root)
+			{
+				num = (num << 1) + root->val;
+				travalQ.push(pair<TreeNode*,int>(root,num));
+				root = root->left;
+			}
+
+			root = travalQ.top().first;
+			num = travalQ.top().second;
+			travalQ.pop();
+			
+			if (!root->left && !root->right)
+			{
+				result += num;
+			}
+
+			root = root->right;
+			
+		}
+		return result;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+        
+        int ret = Solution().sumRootToLeaf(root);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
+
