e047079e73085c46b742f3b89bf8b3198b124c4a
luxiang <luxiang@tongtech.com>
2020-04-19 19:02:04 +0800

    L404.左叶子之和

---
+++ LeetCode/L404.左叶子之和.cpp
@@ -0,0 +1,104 @@
+/******************************************************************************************
+* Data Structures in C++
+* Auther : luxiang
+* github:https://github.com/Lxxing?tab=repositories
+* 欢迎交流指正
+******************************************************************************************/
+#include "LeetCode.h"
+
+class Solution {
+public:
+	void DFS(TreeNode* node,bool left,int &sum)
+	{
+		if (!node)
+		{
+			return;
+		}
+
+		if (left && !node->left && !node->right)
+		{
+			sum += node->val;
+		}
+
+		DFS(node->left,true,sum);
+		DFS(node->right,false,sum);
+
+	}
+    int sumOfLeftLeaves(TreeNode* root) {
+		int sum = 0;
+		DFS(root,true,sum);
+		return sum;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+        
+        int ret = Solution().sumOfLeftLeaves(root);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
+
