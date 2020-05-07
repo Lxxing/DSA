a71f7625f724be359fc54256b821a09036b7188c
luxiang <luxiang@tongtech.com>
2020-05-07 21:23:23 +0800

    L572.另一个树的子树

--- LeetCode/LeetCode.h
+++ LeetCode/LeetCode.h
@@ -30,7 +30,9 @@ struct TreeNode {
 	  int val;
 	  TreeNode *left;
 	  TreeNode *right;
+	  TreeNode() : val(0), left(nullptr), right(nullptr) {}
 	  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+	  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 struct ListNode {
