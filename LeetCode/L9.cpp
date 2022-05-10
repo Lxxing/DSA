332a27b59519d22e8be7b6530a88049199b2678f
luxiang <xiaoxiaomiantianwo@163.com>
2022-05-10 16:27:32 +0800

    L9回文方法二

--- LeetCode/L9.cpp
+++ LeetCode/L9.cpp
@@ -11,10 +11,49 @@
 
 class Solution {
 public:
-    bool isPalindrome(int x) {
+	//
+	bool isPalindrome(int x) {
+		if (x < 0 )
+		{
+			return false;
+		}
+
+		long long convertX = 0;
+		long long dummyX = x;
+		while (dummyX > 0 )
+		{
+			convertX = convertX * 10 + dummyX % 10;
+			dummyX /= 10;
+		}
+
+		return convertX == x;
+	}
+
+	bool isPalindrome1(int x) {
+		//特殊情况.0结尾的整数在后面的算法中会判断错误
+		if (x < 0 || (x % 10 ==0 && x != 0))
+		{
+			return false;
+		}
+
+		//全部反转可能溢出.方法一当convertX位数大于dummyX时,位数超过一半
+		//方法二：使用long long存储大数
+		int convertX = 0;
+		int dummyX = x;
+		while (dummyX > 0 && dummyX > convertX)
+		{
+			convertX = convertX * 10 + dummyX % 10;
+			dummyX /= 10;
+		}
+
+		//
+		return convertX == dummyX || convertX == dummyX * 10 + convertX % 10;
+	}
+
+    bool isPalindrome0(int x) {
 		string result = std::to_string(x);
 		string::iterator beginP = result.begin();
-		string::iterator endP = result.end() - 1;
+		string::iterator endP = result.end() - 1;//end指向哨兵空节点
 		int count = 0;
 		while (beginP < endP && *beginP == *endP)
 		{
